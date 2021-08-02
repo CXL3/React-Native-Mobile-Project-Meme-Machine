6e3a72888aa1be03d582a97474ebeb9615630f3a
Claire Lu <appsfunlu@gmail.com>
2021-08-01 19:45:48 -0700

    added and imported comments.js into comments component

--- components/CommentsComponent.js
+++ components/CommentsComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Card, Image } from "react-native-elements";
+import { Text, View, ScrollView, FlatList } from "react-native";
+import { Card } from "react-native-elements";
 import { HOTMEMES } from "../shared/hotMemes";
+import { COMMENTS } from "../shared/comments";
+import { Divider } from "react-native-elements/dist/divider/Divider";
 
-function RenderComments({ hotMeme }) {
+function RenderMeme({ hotMeme }) {
   if (hotMeme) {
     return (
       <Card>
@@ -11,17 +13,42 @@ function RenderComments({ hotMeme }) {
         <Card.Divider />
         <Card.Image source={require("./images/soup.png")}></Card.Image>
         {/* <Image source={require("./images/soup.png")}></Image> */}
-        <Text style={{ margin: 10 }}>{hotMeme.comments}</Text>
       </Card>
     );
   }
   return <View />;
 }
+function RenderComments({ comments }) {
+  const renderCommentItem = ({ item }) => {
+    return (
+      <View style={{ margin: 10 }}>
+        <Text style={{ fontSize: 14 }}>{item.text}</Text>
+
+        <Text
+          style={{ fontSize: 12 }}
+        >{`-- ${item.author}, ${item.date}`}</Text>
+        <Divider />
+      </View>
+    );
+  };
+
+  return (
+    <Card title="Comments">
+      <FlatList
+        data={comments}
+        renderItem={renderCommentItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </Card>
+  );
+}
+
 class Comments extends Component {
   constructor(props) {
     super(props);
     this.state = {
       hotMemes: HOTMEMES,
+      comments: COMMENTS,
     };
   }
   static navigationOptions = {
@@ -32,7 +59,15 @@ class Comments extends Component {
     const hotMeme = this.state.hotMemes.filter(
       (hotMeme) => hotMeme.id === hotMemeId
     )[0];
-    return <RenderComments hotMeme={hotMeme} />;
+    const comments = this.state.comments.filter(
+      (comment) => comment.hotMemeId === hotMemeId
+    );
+    return (
+      <ScrollView>
+        <RenderMeme hotMeme={hotMeme} />
+        <RenderComments comments={comments} />
+      </ScrollView>
+    );
   }
 }
 
